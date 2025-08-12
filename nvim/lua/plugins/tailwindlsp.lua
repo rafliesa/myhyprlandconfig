1cdff0f01f8f702f85978a949c8489ae99236515
rafliesa <rflisaa@gmail.com>
2025-08-12 08:19:32 +0700

    Inital commit

---
+++ nvim/lua/plugins/tailwindlsp.lua
@@ -0,0 +1,12 @@
+return {
+  -- Example in lazy.lua or a dedicated plugin file
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- ... other options
+      extras = {
+        "lang.tailwind", -- Enable Tailwind CSS extra
+      },
+    },
+  },
+}
